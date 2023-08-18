58db6fe7b815f7eeae7090bd8f0d016672b9ccf4
Saad <70136884+saadtahir995@users.noreply.github.com>
2023-08-18 19:49:45 +0500

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,9 +1,4 @@
 import mysql2 from 'mysql2/promise'
-
-console.log("HOST:", process.env.HOST);
-console.log("USER:", process.env.USER);
-console.log("PWD:", process.env.PASSWORD);
-console.log("DB:", process.env.DB);
 const conn= mysql2.createPool({
     host: process.env.HOST,
     user: process.env.USER,
