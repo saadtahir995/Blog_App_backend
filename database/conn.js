68d3d7cec6c76b037d0809fa2dd98aba93d79eef
Saad <70136884+saadtahir995@users.noreply.github.com>
2023-08-18 19:08:28 +0500

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,9 +1,9 @@
 import mysql2 from 'mysql2/promise'
 const conn= mysql2.createPool({
-    host: 'b92d5el9jwxw5ut28zdy-mysql.services.clever-cloud.com',
-    user: 'uqkuuw4lup3e5m4i',
-    password: 'v3krqx0LWLwz9bjsRZkM',
-    database: 'b92d5el9jwxw5ut28zdy',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PWD,
+    database: process.env.DB,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
