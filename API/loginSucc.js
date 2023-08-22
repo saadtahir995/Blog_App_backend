5f8c1f685c4fb32fe1b3e08fe5044115a7f76c6f
saadtahir995 <saadtahir995@yahoo.com>
2023-08-22 13:32:02 +0500

    final commit

--- API/loginSucc.js
+++ API/loginSucc.js
@@ -12,7 +12,7 @@ app.post('/login',Middleware, async(req, res) => {
         username:req.user
     }
       const token= jwt.sign(user,Secret_key);
-      res.cookie('token',token,{ maxAge: 86400000, httpOnly: true });
+      res.cookie('token',token,{ maxAge: 86400000, httpOnly: true,sameSite: 'None' });
     return res.status(200).json({
         message: 'Login Successful',
         isLoggedin: true,
