57eea62827df1e7c13cdb0806036a7e2094f3ae9
saadtahir995 <saadtahir995@yahoo.com>
2023-08-22 13:23:11 +0500

    final commit

--- API/loginSucc.js
+++ API/loginSucc.js
@@ -2,14 +2,17 @@ import express from 'express'
 import config from '../config/config.js'
 import Middleware from '../Middleware/loginAuth.js'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 const app = express();
 app.use(config);
+app.use(cookieParser());
 const Secret_key=process.env.SECRET_KEY
 app.post('/login',Middleware, async(req, res) => {
     const user={id:req.id,
         username:req.user
     }
       const token= jwt.sign(user,Secret_key);
+      res.cookie('token',token,{ maxAge: 86400000, httpOnly: true });
     return res.status(200).json({
         message: 'Login Successful',
         isLoggedin: true,
