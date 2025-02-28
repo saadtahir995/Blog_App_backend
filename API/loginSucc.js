2a6ca035094cccf41d5a0776c14aa179f96c89f6
Saad <70136884+saadtahir995@users.noreply.github.com>
2025-03-01 00:29:39 +0500

    Update loginSucc.js

--- API/loginSucc.js
+++ API/loginSucc.js
@@ -19,4 +19,29 @@ app.post('/login',Middleware, async(req, res) => {
         token:token
     });
 });
+
+// Guest login endpoint
+app.post('/guest-login', async(req, res) => {
+    const guestId = 'guest_' + Math.random().toString(36).substr(2, 9);
+    const user = {
+        id: guestId,
+        username: 'Guest_' + guestId.slice(-4),
+        isGuest: true
+    }
+    const token = jwt.sign(user, Secret_key, { expiresIn: '24h' });
+    res.cookie('token', token, { 
+        maxAge: 86400000, // 24 hours
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true 
+    });
+    return res.status(200).json({
+        message: 'Guest Login Successful',
+        isLoggedin: true,
+        isGuest: true,
+        username: user.username,
+        token: token
+    });
+});
+
 export default app;
